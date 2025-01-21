096ad487446e04c5481a4ab49bccfdc6d0177147
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2025-01-21 01:41:31 -0500

    style changes and package impl

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import FlipClock from './FlipClock';
-import './FlipClock.css';
-
+import React from "react";
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+import "./App.css"; // Your custom styles
 
 const App = () => {
   return (
-    <div>
-      <FlipClock targetDateTime="2025-03-03T08:55:00-05:00" />
+    <div style={{ textAlign: "center", color: "white", backgroundColor: "black", height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+      <h1 style={{ fontFamily: "'Comic Sans MS', cursive, sans-serif", fontSize: "2rem", marginBottom: "20px" }}>
+      ✈️🌴🍹🍉⛱️🥥 pack your bags. 🥥⛱️🍉🍹🌴✈️
+      </h1>
+      <FlipClockCountdown
+        to={new Date("2025-03-03T08:55:00-05:00").getTime()}
+        labels={["Days", "Hours", "Minutes", "Seconds"]}
+        className="flip-clock"
+      />
     </div>
   );
 };
