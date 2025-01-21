87e4f2e1340a431476cb7030c33ea03d1f24c07d
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2025-01-21 01:46:01 -0500

    stoopi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div style={{ textAlign: "center", color: "white", backgroundColor: "black", height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
       <h1 style={{ fontFamily: "'Comic Sans MS', cursive, sans-serif", fontSize: "2rem", marginBottom: "20px" }}>
-      ✈️🌴🍹🍉⛱️🥥 pack your bags. 🥥⛱️🍉🍹🌴✈️
+      ✈️🌴🍹🍉⛱️🥥 pack your bags 🥥⛱️🍉🍹🌴✈️
       </h1>
       <FlipClockCountdown
         to={new Date("2025-03-03T08:55:00-05:00").getTime()}
